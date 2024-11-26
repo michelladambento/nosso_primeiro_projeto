22aa2b47d4246d1a5bb4cadb232e989d5692c59d
michelladambento <michelladamb@gmail.com>
2024-11-26 09:54:29 -0300

    progressbar

--- lib/task_item.dart
+++ lib/task_item.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
+
   final String nome;
 
   const Task(this.nome, {super.key});
 
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+
+  int nivel = 0;
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,45 +25,71 @@ class Task extends StatelessWidget {
             color: Colors.blueAccent,
             height: 140,
           ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 72,
-                  height: 100,
-                  color: Colors.black26,
+          Column(
+            children: [
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 72,
+                      height: 100,
+                      color: Colors.black26,
+                    ),
+                    Container(
+                      width: 200,
+                      child: Text(
+                        widget.nome,
+                        style: TextStyle(
+                            fontSize: 20,
+                            overflow: TextOverflow.ellipsis,
+                            color: Colors.blueGrey),
+                      ),
+                    ),
+                    // Container(
+                    //     child: Column(
+                    //         children: [
+                    //           Text(nome),
+                    //           Row(children: [
+                    //             Icon(Icons.star),
+                    //             Icon(Icons.star),
+                    //             Icon(Icons.star),
+                    //             Icon(Icons.star),
+                    //           ])
+                    //         ])
+                    // ),
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            nivel++;
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_up))
+                  ],
                 ),
-                Container(
-                  width: 200,
-                  child: Text(
-                    nome,
-                    style: TextStyle(
-                      fontSize: 20,
-                        overflow:  TextOverflow.ellipsis,
-                      color: Colors.blueGrey
+              ),
 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      child: LinearProgressIndicator(
+                        color: Colors.yellow,
+                        value: nivel/10,
+                      ),
+                      width: 200,
                     ),
                   ),
-                ),
-                // Container(
-                //     child: Column(
-                //         children: [
-                //           Text(nome),
-                //           Row(children: [
-                //             Icon(Icons.star),
-                //             Icon(Icons.star),
-                //             Icon(Icons.star),
-                //             Icon(Icons.star),
-                //           ])
-                //         ])
-                // ),
-                ElevatedButton(onPressed: () {}, child: Icon(Icons.arrow_drop_up))
-              ],
-            ),
+                  Text('Nível $nivel',style: TextStyle(color: Colors.white, fontSize: 16)),
+                ],
+              ),
+
+            ],
           ),
         ]),
       ),
